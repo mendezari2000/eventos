42956ec92a8de2c100759baf43f1c9f05df3d5de
Matias <mati.araujo1998@gmail.com>
2025-06-12 22:34:48 -0300

    fix

--- app/admin.py
+++ app/admin.py
@@ -6,6 +6,7 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "date")
     search_fields = ("title", "date")
     list_filter = ("date",)
+    
 
 
 admin.site.register(Event, EventAdmin)
