b0f6e46e8678a5db1a5f57f471b4b5e37fdb0a96
getJota <eberlejoa@gmail.com>
2025-09-10 00:26:11 -0300

    Correccion en carrusel, navbar(admin) y formulario de eventos

--- panel_admin/views.py
+++ panel_admin/views.py
@@ -38,7 +38,7 @@ class EventListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
 
 class EventCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Event
-    fields = ['title','date','venue','category','description', 'image']
+    fields = ['title','date','venue','category','description', 'image', 'prize']
     template_name = "panel_admin/event_form.html"
     success_url = reverse_lazy('panel_admin:admin_dashboard')
 
@@ -47,7 +47,7 @@ class EventCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 
 class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Event
-    fields = ['title','date','venue','category','description', 'image']
+    fields = ['title','date','venue','category','description', 'image', 'prize']
     template_name = "panel_admin/event_form.html"
     success_url = reverse_lazy('panel_admin:admin_dashboard')
 
